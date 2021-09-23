570cebec1f6264f361b7a0fa64a3eda7a820f640
Navya Rajput <navyasrajput@gmail.com>
2021-09-23 13:46:25 -0400

    client and play in snake-client

---
+++ client.js
@@ -0,0 +1,17 @@
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '135.23.223.133',
+    port: '50542',
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+  module.exports = {
+    connect, // stores the function as myFunction
+  };
--- play.js
+++ play.js
@@ -1,17 +1,6 @@
 const net = require("net");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '135.23.223.133',
-    port: '50542',
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
-
+const { connect } = require("./client.js");
 console.log("Connecting ...");
 connect();
